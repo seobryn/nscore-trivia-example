9ae9895e51fdafc7a4220198ed50fc6d4f9f334f
Jose Joya <jose.joya@globant.com>
2021-01-19 09:52:58 -0500

    Feat: Finished example

--- app/game/main-scene-view-model.ts
+++ app/game/main-scene-view-model.ts
@@ -1,18 +1,26 @@
-import { ListView, Observable, ObservableArray, Page, Repeater } from "@nativescript/core";
+import {  Frame, Observable, Page, PropertyChangeData } from "@nativescript/core";
 import { setInterval, clearInterval} from "@nativescript/core/timer";
 import HttpClient from "~/utils/Http"
-import { keyBy, unescape, union, sortBy } from 'lodash';
+import { keyBy, unescape, union } from 'lodash';
+import { ObservableProperty } from "~/observable-property-decorator";
+import { RadListView } from "nativescript-ui-listview";
+import { ToastDuration, Toasty } from "nativescript-toasty";
 
+declare const android;
+declare const java;
 export class MainSceneViewModel extends Observable {
 
     user_name: string;
     isGameStarted: boolean;
     time: number;
     isTimeVisible: boolean;
+    tries= 3;
+    correctAnswers: number=0;
 
     questions: { [key:string]: any};
     questionCount: number=0;
-    answers: ObservableArray<string>;
+    letters: ['A','B','C','D'];
+    @ObservableProperty() answers: string[];
 
     constructor(name:string, private http:HttpClient, private pageRef:Page) {
         super();
@@ -37,18 +45,51 @@ export class MainSceneViewModel extends Observable {
 
     async loadQuestions(){
         const questions = await this.http.getRandonQuestions();
+        console.log(new java.lang.String(android.util.Base64.decode(questions[0].correct_answer, android.util.Base64.DEFAULT),java.nio.charset.StandardCharsets.UTF_8));
+
+        this.answers = [];
         for(var i=0; i< questions.length; i+=1){
             questions[i].index = i;
-            questions[i].question = unescape(questions[i].question.replace('&#',' &#'));
-            questions[i].answers = union(questions[i].incorrect_answers.map((a,i)=>unescape(a)),[questions[i].correct_answer]).sort();
+            questions[i].question = new java.lang.String(android.util.Base64.decode(questions[i].question, android.util.Base64.DEFAULT),java.nio.charset.StandardCharsets.UTF_8);
+            let index = Math.ceil(((Math.random()*100)+1) % questions[i].incorrect_answers.length);
+            questions[i].answers = questions[i].incorrect_answers.map((a,i)=>new java.lang.String(android.util.Base64.decode(a, android.util.Base64.DEFAULT)));
+            questions[i].answers.splice(index,0,new java.lang.String(android.util.Base64.decode(questions[i].correct_answer, android.util.Base64.DEFAULT)));
+            questions[i].correct_answer = new java.lang.String(android.util.Base64.decode(questions[i].correct_answer, android.util.Base64.DEFAULT));
         }
         this.set('questions',keyBy(questions,'index'));
-        this.questions[this.questionCount].answers.forEach(i=>this.answers.push(i))
+        this.answers = questions[this.questionCount].answers;
     }
 
 
-    answer(option: number){
-
+    answer(response){
+        if(this.answers[response.index] === this.questions[this.questionCount].correct_answer){
+            new Toasty({
+                text:'Correcto!',
+                backgroundColor:"green",
+                duration: ToastDuration.SHORT
+            }).show();
+            this.set('correctAnswers',this.correctAnswers+1);
+        }else {
+            new Toasty({
+                text:'Incorrecto :(',
+                backgroundColor:"red",
+                duration: ToastDuration.SHORT
+            }).show();
+            this.set('tries',this.tries-1);
+        }
+        this.set('questionCount', this.questionCount+1);
+        this.answers = this.questions[this.questionCount].answers;
+        if(this.tries === 0 ){
+            new Toasty({
+                text: `Juego terminado, Puntaje: ${this.correctAnswers}`,
+                backgroundColor:'blue',
+                duration: ToastDuration.LONG,
+            }).show()
+            Frame.topmost().navigate({
+                animated:true,
+                moduleName: 'home/home-page',
+            });
+        }
     }
 
 }
